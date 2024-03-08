6fe90d1052218debd008c6520a064808a0c34211
SantiagoVelasco <05santiagovelasco@gmail.com>
2024-03-08 16:14:48 -0500

    feat(controller): Add rename method and refactor validateFolderData

--- src/controllers/folder.controller.ts
+++ src/controllers/folder.controller.ts
@@ -13,7 +13,8 @@ class FolderController {
                 parentId,
                 createdDate,
                 status,
-                userId
+                userId,
+                "create"
             );
             if (errors.length > 0) {
                 return res.status(400).send({
@@ -52,13 +53,72 @@ class FolderController {
         }
     };
 
+    public rename = async (req: Request, res: Response): Promise<Response> => {
+        try {
+            let errors = [];
+            const { name } = req.body;
+            const userId = req.params.userId;
+            const folderId = req.params.folderId;
+            errors = this.validateFolderData(
+                folderId,
+                name,
+                null,
+                null,
+                null,
+                userId,
+                "rename"
+            );
+            if (errors.length > 0) {
+                return res.status(400).send({
+                    errors: errors,
+                    success: false,
+                    data: null,
+                });
+            }
+            const folder = await FolderModel.findOne({
+                where: {
+                    id: folderId,
+                    user_id: userId,
+                },
+            });
+            if (!folder) {
+                return res.status(404).send({
+                    errors: ["Folder not found"],
+                    success: false,
+                    data: null,
+                });
+            }
+            const update = await folder.update({ name: name });
+            if (update) {
+                return res.status(200).send({
+                    errors: errors,
+                    success: true,
+                    data: update,
+                });
+            }
+            return res.status(500).send({
+                errors: ["Failed to rename folder"],
+                success: false,
+                data: null,
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send({
+                errors: ["Internal server error", error],
+                success: false,
+                data: null,
+            });
+        }
+    };
+
     private validateFolderData = (
         id: any,
         name: any,
         parentId: any,
         createdDate: any,
         status: any,
-        userId: any
+        userId: any,
+        method: string
     ): string[] => {
         const errors: string[] = [];
 
@@ -72,28 +132,30 @@ class FolderController {
         } else if (typeof name !== "string") {
             errors.push("Invalid name");
         }
-        if (parentId) {
-            if (typeof parentId !== "string") {
-                errors.push("Invalid parentId");
-            }
-        }
-        if (!createdDate) {
-            errors.push("Missing createdDate");
-        } else {
-            const parsedDate = new Date(createdDate);
-            if (isNaN(parsedDate.getTime())) {
-                errors.push("Invalid createdDate");
-            }
-        }
-        if (!status) {
-            errors.push("Missing status");
-        } else if (typeof status !== "boolean") {
-            errors.push("Invalid status");
-        }
         if (!userId) {
             errors.push("Missing userId");
         } else if (typeof userId !== "string") {
             errors.push("Invalid userId");
+        }
+        if (method === "create") {
+            if (parentId) {
+                if (typeof parentId !== "string") {
+                    errors.push("Invalid parentId");
+                }
+            }
+            if (!createdDate) {
+                errors.push("Missing createdDate");
+            } else {
+                const parsedDate = new Date(createdDate);
+                if (isNaN(parsedDate.getTime())) {
+                    errors.push("Invalid createdDate");
+                }
+            }
+            if (!status) {
+                errors.push("Missing status");
+            } else if (typeof status !== "boolean") {
+                errors.push("Invalid status");
+            }
         }
         return errors;
     };
