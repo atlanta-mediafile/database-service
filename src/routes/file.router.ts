512ad82596abe36021f0c7b04f87f7c3164a34ef
SantiagoVelasco <05santiagovelasco@gmail.com>
2024-03-14 17:02:45 -0500

    feat(routes): Add FileRouter class
    - Added endpoint for file creation

---
+++ src/routes/file.router.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import FileController from "../controllers/file.controller";
+
+class FileRouter{
+    private _router: Router;
+    private fileController: FileController;
+
+    constructor(){
+        this._router = Router();
+        this.fileController = new FileController();
+        this.config();
+    }
+
+    private config(){
+        this._router.post('/:userId/file', this.fileController.create);
+    }
+
+    public get router(){
+        return this._router;
+    }
+}
+
+export default FileRouter;
