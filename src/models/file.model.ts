d4c431165406ca875e5bfafa5998279fc06f7c2a
SantiagoVelasco <05santiagovelasco@gmail.com>
2024-03-14 16:54:45 -0500

    feat(models): Add FileModel

---
+++ src/models/file.model.ts
@@ -0,0 +1,74 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../database/dbConnection";
+
+interface FileAtributes {
+    id: string;
+    name: string;
+    user_id: string;
+    extension: string;
+    mime_type: string;
+    size: number;
+    folder_id: string | null;
+    created_date: Date;
+    status: boolean;
+}
+
+class FileModel extends Model<FileAtributes> implements FileAtributes {
+    id!: string;
+    name!: string;
+    user_id!: string;
+    extension!: string;
+    mime_type!: string;
+    size!: number;
+    folder_id!: string | null;
+    created_date!: Date;
+    status!: boolean;
+}
+
+FileModel.init(
+    {
+        id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+        },
+        extension: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        mime_type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        size: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        folder_id: {
+            type: DataTypes.UUID,
+            allowNull: true,
+        },
+        created_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        tableName: 'file'
+    }
+);
+
+export default FileModel;
