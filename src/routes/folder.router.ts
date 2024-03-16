b4f5974c0369470c785db693502eb9e43560ddcc
SantiagoVelasco <05santiagovelasco@gmail.com>
2024-03-16 15:48:31 -0500

    feat (routes): add endopoint to get folders and files from a folder in FolderRouter

--- src/routes/folder.router.ts
+++ src/routes/folder.router.ts
@@ -15,6 +15,7 @@ class FolderRouter{
         this._router.post('/:userId/folder', this.folderController.create);
         this._router.put('/:userId/folder/:folderId', this.folderController.rename);
         this._router.delete('/:userId/folder/:folderId', this.folderController.delete);
+        this._router.get('/:userId/folder/:folderId', this.folderController.getFilesAndFoldersFromAFolder);
     }
 
     public get router(){
