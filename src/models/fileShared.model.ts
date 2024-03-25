f16c9c2de673fd0034f06388a8a3400dfe1e5599
SantiagoVelasco <05santiagovelasco@gmail.com>
2024-03-25 16:44:41 -0500

    feat(models): add FileSharedModel

---
+++ src/models/fileShared.model.ts
@@ -0,0 +1,48 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../database/dbConnection";
+
+interface FileSharedAtributes {
+    id: number | null;
+    file_id: string;
+    user_id: string;
+    status: boolean;
+}
+
+class FileSharedModel
+    extends Model<FileSharedAtributes>
+    implements FileSharedAtributes
+{
+    id!: number | null;
+    file_id!: string;
+    user_id!: string;
+    status!: boolean;
+}
+
+FileSharedModel.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        file_id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        tableName: "file_shared",
+    }
+);
+
+export default FileSharedModel;
