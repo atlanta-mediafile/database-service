e52ac9777984ffdf43c88266064120774a2f539d
SantiagoVelasco <05santiagovelasco@gmail.com>
2024-03-26 11:18:36 -0500

    chore(database): add function verify_new_folder_is_child_of_current_folder

--- src/database/create_database.sql
+++ src/database/create_database.sql
@@ -36,3 +36,26 @@ CREATE TABLE folder_shared (
     user_id UUID NOT NULL,
     status BOOLEAN NOT NULL
 );
+
+CREATE OR REPLACE FUNCTION verify_new_folder_is_child_of_current_folder(current_id UUID, new_id UUID)
+RETURNS BOOLEAN AS $$
+DECLARE
+    is_child BOOLEAN;
+    parent_folder_id UUID;
+BEGIN
+    is_child := FALSE;
+    parent_folder_id := new_id;
+    LOOP
+        SELECT f.parent_id INTO parent_folder_id FROM folder f WHERE f.id = parent_folder_id;
+        IF parent_folder_id IS NULL THEN
+            EXIT;
+        END IF;
+        IF parent_folder_id = current_id THEN
+            is_child := TRUE;
+            EXIT;
+        END IF;
+    END LOOP;
+    
+    RETURN is_child;
+END;
+$$ LANGUAGE plpgsql;
